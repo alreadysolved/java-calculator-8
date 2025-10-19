56562f7af91a7e8c352da85a2872c5ec3673ef4e
권영은 <window@MacBook-Air.local>
2025-10-19 20:51:49 +0900

    feat: 구분자 설정 기능 추가
    기본 구분자와 커스텀 구분자를 추가하여 전체 구분자 설정

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,19 @@
 package calculator;
+import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String line = Console.readLine();
+        String sep = ":|,";
+        long result = 0L;
+
+        // 커스텀 구분자 설정
+        if (line.startsWith("//")) {
+            String customSep = line.substring(2);
+            customSep = java.util.regex.Pattern.quote(customSep);
+            sep = sep + "|" + customSep;
+            line = Console.readLine();
+        }
     }
 }
